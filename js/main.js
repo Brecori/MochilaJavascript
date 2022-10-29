e9dddd152ace51d2bc210708bb91a8625df84e74
Breno Tosi <101286015+Brecori@users.noreply.github.com>
2022-10-28 22:24:15 -0300

    Adicionando na Local Storage

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = [];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nome = e.target.elements["nome"];
+    const quantidade = e.target.elements["quantidade"];
+
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value = "";
+    quantidade.value = "";
+})
+
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item")
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+    
+    itens.push(itemAtual)
+
+    localStorage.setItem('item', JSON.stringify(itens));
+    
+
+}
